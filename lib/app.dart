cf932691941d8b54ab275f49bb2c0f40a74a500e
Navin1999123 <navinkumarnkh@gmail.com>
2025-09-08 14:13:27 +0530

    optimize code

--- lib/app.dart
+++ lib/app.dart
@@ -2,13 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movie_app/bloc/movie_bloc/movie_bloc.dart';
 import 'package:movie_app/data/repository/movie_repository.dart';
-import 'package:movie_app/prasentation/splash/splash_screen.dart';
 import 'package:movie_app/utils/app_routes/page_router.dart';
 import 'package:movie_app/utils/app_routes/page_routes.dart';
 
 class MovieApp extends StatelessWidget {
-  final MovieRepository movieRepository;
-  const MovieApp({required this.movieRepository, super.key});
+  final MovieRepository? movieRepository;
+  const MovieApp({this.movieRepository, super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +22,7 @@ class MovieApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           initialRoute: AppRoutes.splash,
           onGenerateRoute: AppRouter(
-            movieRepository: movieRepository,
+            movieRepository: movieRepository!,
           ).onGenerateRoute,
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,24 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:hive_flutter/adapters.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:movie_app/app.dart';
 import 'package:movie_app/data/modal/movie.dart';
 import 'package:movie_app/data/repository/movie_repository.dart';
 import 'package:movie_app/data/source/local_db_source.dart';
 import 'package:movie_app/data/source/tmdb_api_source.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: "assets/.env");
   await Hive.initFlutter();
   Hive.registerAdapter(MovieAdapter());
-  await Hive.openBox<Movie>('movies');
-  await Hive.openBox<Movie>('bookmarks');
-  await Hive.openBox('settingsBox');
+  await Future.wait([
+    Hive.openBox<Movie>('movies'),
+    Hive.openBox<Movie>('bookmarks'),
+    Hive.openBox('settingsBox'),
+  ]);
+
   final dio = Dio();
   final tmdbApi = TMDBApiSource(dio, baseUrl: "https://api.themoviedb.org/3");
   final localDb = LocalDBSource();
